bf1e376c3c1e0a640d0fa0ced1a2beca7db1fb0c
reber0 <reber0ask@qq.com>
2023-11-22 16:14:27 +0800

    update

--- mylog2.go
+++ mylog2.go
@@ -2,12 +2,13 @@
  * @Author: reber
  * @Mail: reber0ask@qq.com
  * @Date: 2023-10-12 17:55:40
- * @LastEditTime: 2023-10-12 17:55:57
+ * @LastEditTime: 2023-11-22 16:13:46
  */
 package goutils
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/mattn/go-colorable"
 )
@@ -23,17 +24,21 @@ const (
 )
 
 func Red(msg string) {
-	fmt.Fprintf(stdout, "%s%s%s\n", red, msg, reset)
+	t := time.Now().Format("15:04:05")
+	fmt.Fprintf(stdout, "[%s] %s%s%s\n", t, red, msg, reset)
 }
 
 func Green(msg string) {
-	fmt.Fprintf(stdout, "%s%s%s\n", green, msg, reset)
+	t := time.Now().Format("15:04:05")
+	fmt.Fprintf(stdout, "[%s] %s%s%s\n", t, green, msg, reset)
 }
 
 func Yellow(msg string) {
-	fmt.Fprintf(stdout, "%s%s%s\n", yellow, msg, reset)
+	t := time.Now().Format("15:04:05")
+	fmt.Fprintf(stdout, "[%s] %s%s%s\n", t, yellow, msg, reset)
 }
 
 func Blue(msg string) {
-	fmt.Fprintf(stdout, "%s%s%s\n", blue, msg, reset)
+	t := time.Now().Format("15:04:05")
+	fmt.Fprintf(stdout, "[%s] %s%s%s\n", t, blue, msg, reset)
 }
